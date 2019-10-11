62c0aa3e7242c4dceeb932dc5ccec0d3b04d0112
Chris Gartsen <chris@gartsen.nl>
2019-10-11 11:50:34 +0200

    Return jwt on login

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,19 +1,16 @@
 const User = require('../models/user')
 
 module.exports.login = async(req, res, next) => {
-
   const email = req.body.email
   const password = req.body.password
-
   if(!email || !password) return res.status(422).json({ error: 'Credentials are missing' })
-
   try {
     const user = await User.findOne({ email: email }).select("+password")
-    console.log(user)
     if(!user) return res.status(401).json({ error: 'Authentication failed' })
     const valid = await user.checkCredentials(password)
     if(valid) {
-      res.status(200).json({ message: "Hurray" }) // @TODO -> Return JWT
+      const token = await user.generateToken()
+      res.status(200).header('x-auth-token', token).json({ token }) 
     } else {
       res.status(401).json({ error: 'Authentication failed' })
     }
--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -24,6 +25,23 @@ userSchema.plugin(uniqueValidator)
 
 userSchema.methods.checkCredentials = async function(password) {
   return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateToken = async function() {
+  try {
+    return await jwt.sign({ data: this._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  } catch(err) {
+    throw(err)
+  }
+}
+
+userSchema.methods.verifyToken = async function(token) {
+  try {
+    const decoded = await jwt.verify(token, process.env.JWT_SECRET )
+    console.log(decoded)
+  } catch(err) {
+    console.log(err)
+  }
 }
 
 userSchema.pre('save', async function(){
