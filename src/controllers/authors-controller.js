e34b0b50b3bd8761ea3b2d404ff503db08cadf9c
Chris Gartsen <chris@gartsen.nl>
2019-10-16 18:13:22 +0200

    Add get authors actions

--- src/controllers/authors-controller.js
+++ src/controllers/authors-controller.js
@@ -10,11 +10,22 @@ module.exports.delete = async (req, res, next) => {
 }
 
 module.exports.getAll = async (req, res, next) => {
-
+  try {
+    const authors = await Author.find()
+    res.status(200).json({ authors })
+  } catch(err) {
+    next(err)
+  }
 }
 
 module.exports.getOne = async (req, res, next) => {
-
+  try {
+    const author = await Author.findById(req.params.id)
+    if(!author) return res.status(404).json({ error: 'Author not found'})
+    res.status(200).jso({ author })
+  } catch(err) {
+    next(err)
+  }
 }
 
 module.exports.update = async (req, res, next) => {
