2f42d8f20c31d4cde0a69217784f4105f4fa68c8
Chris Gartsen <chris@gartsen.nl>
2019-10-23 18:15:44 +0200

    Add Cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
+const cors = require('cors')
 
 const auth = require('./controllers/auth-controller')
 
@@ -15,6 +16,7 @@ require('./config/database').connectDB()
 const app = express()
 
 app.use(helmet())
+app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api', auth.verifyToken)
